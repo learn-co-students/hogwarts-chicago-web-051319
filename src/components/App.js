71e718938e9f46739ca006cfc70b6a4413aa60ad
github username <github email address>
2019-07-18 17:57:50 -0500

    MVP

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,72 @@ import '../App.css';
 import Nav from './Nav'
 import hogs from '../porkers_data';
 
+
+import HogTile from './HogTile'
+import Filter from './Filter'
+
+
+
 class App extends Component {
+
+
+  constructor() {
+    super()
+    this.state={
+      greased: false,
+      sortedBy: ""
+    }
+  }
+
+  receiveFilter = (greaseInfo, sortedByInfo) => {
+    this.setState({
+      greased: greaseInfo,
+      sortedBy: sortedByInfo,
+
+    })
+  }
+
+
+
+  generateHogTiles = () => {
+    let allHogs = hogs
+    let weight = "weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water"
+    {this.state.greased === true ? allHogs=hogs.filter(hog => hog.greased === true) : allHogs=hogs}
+    {this.state.sortedBy === "name" ? allHogs.sort((a, b) => a.name.localeCompare(b.name)) : allHogs.sort((a, b) => a[weight] - b[weight])}
+    return allHogs.map((data, index) =>
+    <HogTile
+      handleHide = {this.handleHide}
+      key={index}
+      image={require(`../hog-imgs/${data.name.toLowerCase().replace(/ /g,"_")}.jpg`)}
+      name={data.name}
+      specialty={data.specialty}
+      greased={data.greased}
+      details={data["weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water"]}
+      medal={data["highest medal achieved"]}
+    />)
+  }
+
+
+    //
+    // handleHide = (event) => {
+    //   let hiddenHog=parseInt(event.target.parentNode.id)
+    //  console.log(this.state.allHogs);
+    //
+    // }
+
+
+
+
   render() {
     return (
       <div className="App">
           < Nav />
+          < Filter receiveFilter = {this.receiveFilter}/>
+
+
+          <div className="ui grid container">
+          {this.generateHogTiles()}
+          </div>
 
       </div>
     )
---
+++ src/components/Filter.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+
+export default class Filter extends Component{
+  constructor(){
+    super()
+    this.state={
+      greased: false,
+      sortedBy: ""
+    }
+  }
+
+
+  handleGreasedChecked = () => {
+   if (this.state.greased === false){
+    this.setState({
+      greased: true
+    })}else{
+      this.setState({
+        greased: false
+      })
+    }
+  }
+
+
+  handleSort = (event) => {
+     let sortValue = event.target.value
+     this.setState({
+       sortedBy: sortValue
+     })
+  }
+
+
+  render(){
+    let greased = this.state.greased
+    let sorted = this.state.sortedBy
+    return(
+      <div>
+       Greased: <input type="checkbox" name="greased" onChange={() => this.handleGreasedChecked()}/>
+       Sort by:
+       <select name="sort" onChange={(event) => this.handleSort(event)}>
+       <option value="none"></option>
+        <option value="name">Name</option>
+        <option value="weight">Weight</option>
+       </select>
+       <button onClick={()=>this.props.receiveFilter(greased, sorted)}>Submit</button>
+
+
+      </div>
+    )
+  }
+
+}
